ecdc6441cd4da73ce07433d03d9e12461ca6df60
xodus5767 <57379582+xodus5767@users.noreply.github.com>
2019-12-20 09:55:11 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,112 +1,95 @@
-let img; 
-let numSegments = 10;
-let value = 0;
-let myCanvas;
-let detailY;
-let xCor = [];
-let yCor = [];
-let bX, bY;
-let vX, vY;
-
-const xStart = 0;
-const yStart = 250;
-
-var r = 0;
-var b = 255;
-var stars = [];
-var speed;
+var ball_diameter = 20;
+var bomb_diameter = 10;
+var xpoint;
+var ypoint;
+var zapperwidth = 6;
+var numofbombs = 20;
+var bombposX =[];
+var bombposY = [];
+var bombacceleration = [];
+var bombvelocity = [];
+var time = 0;
+var timeperiod = 0;
+var score = 0;
+var posX;
 
 function setup() {
-
-  createCanvas(600, 400);
-  background('rgba(0,255,0, 0.25)');
-  frameRate(100);
-  stroke(255,200);
-  fill(250,200,200,50);
-  rect(mouseX, mouseY,25,25);
-  strokeWeight(10);
-  for (var i = 0; i < 600; i++) {
-    stars[i] = new Star();
-      textSize(width/30);
-  textAlign(CENTER, CENTER);
-    textSize(width/30);
-  textAlign(CENTER, CENTER);
+  var temp00 =0, temp01 = -20;
+  while(temp01 < height){
+    temp00 += 0.02;
+    temp01 += temp00;
+    timeperiod++;
   }
+  
+  posX = zapperwidth + 0.5*ball_diameter - 2;
+  xpoint = 0.5 * width;
+  ypoint = height - 0.5*ball_diameter + 1;
+  
+  initbombpos();
 }
-  
-  
-
 function draw() {
-   speed = map(mouseX, 0, width, 0, 70);
-  background(r, 0, b);
-  translate(width / 0, height / 2);
-  for (var i = 0; i < stars.length; i++) {
-    stars[i].update();
-    stars[i].show(); 
-  r = map(mouseX, 0, 600, 0, 50);
-  b = map(mouseX, 0, 600, 50, 0);
-    line(mouseX, mouseY, -290, -190);
-
-  strokeWeight(1);
-  print(pmouseX + ' -> ' + mouseX);
-  fill(value);
-  rect(0, 0, 50, 50);
-    fill(value);
-  rect(550, 350, 50, 50);
-
-    
+  background(51);
+  createCanvas(500,500);
+  fill(239, 58, 38);
+  rect(0, 0, zapperwidth, height);
+  scoreUpdate();
+  fill(0, 255, 0)
+  for(var i=0; i<numofbombs; i++){
+    ellipse(bombposX[i], bombposY[i], bomb_diameter, bomb_diameter);
   }
+  updatebombpos();
+  fill(0, 255, 0)
+    ellipse(xpoint, ypoint, ball_diameter, ball_diameter);
+  xpoint -=1;
+  
+  if(mouseIsPressed && (xpoint + 0.5 *ball_diameter) <width){
+    xpoint +=6;
+  }
+  
+  if(xpoint < posX || bombCollisionTest()){
+    gameover();
+  }
+  time++;
 }
 
-function Star() {
-  this.x = random(-width, width);
-  this.y = random(-height, height);
-  this.z = random(width);
-  this.pz = this.z;
-
-  this.update = function() {
-    this.z = this.z - speed;
-    if (this.z < 1) {
-      this.z = width;
-      this.x = random(-width, width);
-      this.y = random(-height, height);
-      this.pz = this.z;
+function initbombpos(){
+  for(var i=0; i<numofbombs; i++){
+    bombacceleration[i] = random(0.02, 0.03);
+    bombvelocity[i] = random(0, 5);
+    bombposX[i] = random(zapperwidth+(0.5*ball_diameter), width);
+    bombposY[i] = random(-20, -0.5*ball_diameter);
+  }
+}
+function updatebombpos(){
+  
+  for(var i=0; i<numofbombs; i++){
+    bombvelocity[i] += bombacceleration[i];
+    bombposY[i] += bombvelocity[i];
+  }
+  if(time > timeperiod){
+    initbombpos();
+    time = 0;
+  }
+}
+function bombCollisionTest(){
+  var temp = 0.5*(ball_diameter+bomb_diameter)-2;
+  var distance;
+  
+  for(var i=0; i<numofbombs; i++){
+    distance = dist(xpoint, ypoint, bombposX[i], bombposY[i]);
+    if(distance < temp){
+      return true;
     }
   }
-
-  this.show = function() {
-    fill(255);
-    noStroke();
-
-    var sx = map(this.x / this.z, 0, 1, 0, width);
-    var sy = map(this.y / this.z, 0, 1, 0, height);
-
-    var r = map(this.z, 0, width, 10, 0);
-    ellipse(sx, sy, r, r);
-
-    var px = map(this.x / this.pz, 0, 1, 0, width);
-    var py = map(this.y / this.pz, 0, 1, 0, height);
-
-    this.pz = this.z;
-
-    stroke(255);
-    line(px, py, sx, sy);
-
-  }
+  return false;
 }
-
-function dragSegment(i, xin, yin) {
-  const dx = xin - x[i];
-  const dy = yin - y[i];
-  const angle = atan2(dy, dx);
-  x[i] = xin - cos(angle) * segLength;
-  y[i] = yin - sin(angle) * segLength;
-  segment(x[i], y[i], angle);
+function gameover(){
+  fill(225);
+  text("GAME OVER", 0.5*width, 0.5*height);
+  noLoop(); 
 }
-function mouseMoved() {
-  value = value + 50;
-  if (value > 255) {
-    value = 0;
-  }
+function scoreUpdate(){
+  score += 10;
+  fill(225)
+  text("SCORE: " + int(score/timeperiod), width - 65, 10);
 }
-
